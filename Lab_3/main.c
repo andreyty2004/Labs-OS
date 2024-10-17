7eec652ec59ab98a8776ac71f43cdb15bde92245
andreyty2004 <mr.nano.2004@gmail.com>
2024-10-17 19:40:51 +0300

    few changes

--- Lab_3/main.c
+++ Lab_3/main.c
@@ -7,9 +7,9 @@
 #include <sys/wait.h>
 #include <signal.h>
 
-void at_exit()
+void at_exit_handler()
 {
-    printf("[AT_EXIT] at_exit called for %d process\n", getpid());
+    printf("[AT_EXIT] atexit called for %d process\n", getpid());
 }
 
 void signal_handler(int sig)
@@ -20,38 +20,35 @@ void signal_handler(int sig)
 void sigaction_handler(int sig, siginfo_t* sig_info, void* temp)
 {
     printf("[SIGACTION HANDLER] INFO:\n"
-        "Signal number: %d\n Signal code: %d\n"
-        "Sending pid: %d\n Sending uid: %d\n"
-        "Exit value: %d\n User time: %ld\n",
+        "Signal number: %d\nSignal code: %d\n"
+        "Sending pid: %d\nSending uid: %d\n"
+        "Exit value: %d\n",
         sig_info->si_signo, sig_info->si_code, 
         sig_info->si_pid, sig_info->si_uid,
-        sig_info->si_status, sig_info->si_utime);
+        sig_info->si_status);
 }
 
-int main(int argc, char** argv)
+int main()
 {
-    (void) argc;
-    (void) argv;
+    atexit(at_exit_handler); 
+    signal(SIGINT, signal_handler); 
 
-    at_exit(at_exit);
-    signal(SIGINT, signal_handler);
-
-    struct sigaction act;
-    act.sa_flags = SA_SIGINFO;
-    act.sa_sigaction = sigaction_handler;
+    struct sigaction act; 
+    act.sa_flags = SA_SIGINFO; 
+    act.sa_sigaction = sigaction_handler; 
     sigaction(SIGTERM, &act, NULL);
 
-    int res = 0;
+    pid_t res = 0;
     switch(res = fork()) 
     {
         case -1:
-                fprintf(stderr, "Fork error: %s (%d)\n", strerror(errno), errno); /* произошла ошибка */
+                fprintf(stderr, "Fork error: %s (%d)\n", strerror(errno), errno); 
                 break;
         case 0:
                 printf("[CHILD] I'm child of %d, my pid is %d\n", getppid(), getpid());
                 break;
         default:
-                int _res;
+                pid_t _res;
                 wait(&_res);
                 while(1){};
                 printf("[PARENT] I'm parent of %d, my pid is %d, my parent pid is %d\n", res, getpid(), getppid());
